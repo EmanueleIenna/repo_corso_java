6c17677af8126bddb266c6e506ec29264973c1d2
Giuseppe Longo <g.longo@dstech.it>
2025-01-23 17:14:07 +0100

    Creazione cartella studenti per test personali con main personale ma file pom.xml in comune

---
+++ Application/ApplicativoCorsoJava/src/main/java/it/dst/formazione/studenti/LongoGiuseppe/Main.java
@@ -0,0 +1,21 @@
+package it.dst.formazione.studenti.LongoGiuseppe;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        // test_variabili();
+
+        // test_operatori();
+
+        // test_iterazione_while();
+
+        // test_iterazione_for();
+
+        // test_inputoutput();
+
+    }
+}
