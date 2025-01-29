9c5c00b2b550da8685355e683f3ba2c9079f28cf
Alfredo castaldi <alfredcastaldi1992@gmail.com>
2025-01-29 13:44:35 +0100

    esercizio stream manca punto 3

--- Application/ApplicativoCorsoJava/src/main/java/it/dst/formazione/Main.java
+++ Application/ApplicativoCorsoJava/src/main/java/it/dst/formazione/Main.java
@@ -1,10 +1,11 @@
 package it.dst.formazione;
 
+import it.dst.formazione.lezione_8.esercizio.CastaldiAlfredo;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-
+        CastaldiAlfredo.mainAlfredo();
     }
 }
