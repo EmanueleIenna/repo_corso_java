9b59630600f18dea258fbc53b0b451e7b158945f
Valerio <v.alunno@dstech.it>
2025-01-29 12:45:50 +0100

    ValerioAlunno esercizi

---
+++ Application/Esercitazione24-01/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.example.esercitazione1.Esercitazione1.test;
+import static org.example.Student.Studenti;
+import static org.example.Student.libreria;
+public class Main  {
+    public static void main (String[] args) {
+
+        test();
+        Studenti();
+        libreria();
+        }
+    }
